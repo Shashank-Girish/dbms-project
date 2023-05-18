96f9bb884d3e5854d8befd7bfb9d6f1cb657b2c3
Anirudh V Gubbi <anirudhvgubbi@gmail.com>
2023-05-18 22:28:21 +0530

    added logo on the login page

--- lib/features/login/login_page.dart
+++ lib/features/login/login_page.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:vehicle_rental/connector/user_connector.dart';
 import 'package:vehicle_rental/core/colors.dart';
@@ -29,127 +30,151 @@ class _LoginPageState extends State<LoginPage> {
                   begin: Alignment.topCenter,
                   end: Alignment.bottomCenter,
                   colors: kLinearGradientColors)),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(
-                child: Image.asset(
-                  "assets/car.png",
-                ),
-              ),
-              Expanded(
-                child: Form(
-                  key: formKey,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        "Sign In",
-                        style:
-                            TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 50.0),
-                        child: InputTextField(
-                          textEditingController: emailController,
-                           validator: (enteredText) {
-                            return enteredText!.trim().isEmpty;
-                          },
-                          errorColor: kLoginErrorColor,
-                          maxLength: 50,
-                          width: MediaQuery.of(context).size.width * 0.3,
-                          hintText: "Email",
-                          validatorMessage: "Email ID cannot be empty",
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.25),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 40.0),
+                          child: Transform.rotate(
+                            angle: -pi / 30,
+                            child: const Text(
+                              "Motor 4 Road",
+                              style: TextStyle(
+                                fontSize: 80,
+                                fontFamily: 'Neonderthaw',
+                                color: Color.fromRGBO(115, 241, 158, 1),
+                              ),
+                            ),
+                          ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 20.0, bottom: 8.0),
-                        child: InputTextField(
-                          textEditingController: passwordController,
-                          validator: (enteredText) {
-                            return enteredText!.trim().length < 8;
-                          },
-                          errorColor: kLoginErrorColor,
-                          maxLength: 20,
-                          width: MediaQuery.of(context).size.width * 0.3,
-                          hintText: "Password",
-                          validatorMessage: "Password must be atleast 8 characters long",
+                        Image.asset(
+                          "assets/car.png",
                         ),
-                      ),
-                      Row(
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Form(
+                      key: formKey,
+                      child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           const Text(
-                            "Don't have an account? "
+                            "Sign In",
+                            style:
+                                TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
                           ),
-                          MouseRegion(
-                            cursor: SystemMouseCursors.click,
-                            child: GestureDetector(
-                              onTap: () {
-                                // Navigate to Sign Up page
+                          Padding(
+                            padding: const EdgeInsets.only(top: 50.0),
+                            child: InputTextField(
+                              textEditingController: emailController,
+                               validator: (enteredText) {
+                                return enteredText!.trim().isEmpty;
                               },
-                              child: const Text(
-                                "Sign Up!",
-                                style: TextStyle(
-                                  color: Colors.blue,
-                                  fontWeight: FontWeight.w600
+                              errorColor: kLoginErrorColor,
+                              maxLength: 50,
+                              width: MediaQuery.of(context).size.width * 0.3,
+                              hintText: "Email",
+                              validatorMessage: "Email ID cannot be empty",
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20.0, bottom: 8.0),
+                            child: InputTextField(
+                              textEditingController: passwordController,
+                              validator: (enteredText) {
+                                return enteredText!.trim().length < 8;
+                              },
+                              errorColor: kLoginErrorColor,
+                              maxLength: 20,
+                              width: MediaQuery.of(context).size.width * 0.3,
+                              hintText: "Password",
+                              validatorMessage: "Password must be atleast 8 characters long",
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text(
+                                "Don't have an account? "
+                              ),
+                              MouseRegion(
+                                cursor: SystemMouseCursors.click,
+                                child: GestureDetector(
+                                  onTap: () {
+                                    // Navigate to Sign Up page
+                                  },
+                                  child: const Text(
+                                    "Sign Up!",
+                                    style: TextStyle(
+                                      color: Colors.blue,
+                                      fontWeight: FontWeight.w600
+                                    ),
+                                  )
                                 ),
                               )
-                            ),
-                          )
-                        ],
-                      ),
-                      ValueListenableBuilder(
-                        valueListenable: submitted,
-                        builder: (BuildContext context, bool value, _) {
-                          return Padding(
-                            padding: const EdgeInsets.only(top: 20),
-                            child: SolidTextButton(
-                              text: "Sign In",
-                              isLoading: value,
-                              onPressed: () async {
-                                if(value) return;
-                                submitted.value = true;
-
-                                if(!formKey.currentState!.validate()) {
-                                  submitted.value = false;
-                                  return;
-                                }
-
-                                User? user = await UserRemoteDatasource().signIn(User(
-                                  emailId: emailController.text,
-                                  password: passwordController.text
-                                ));
-                                submitted.value = false;
-                                print(user?.name);
-                                userName.value = user?.name;
-                
-                              },
-                            ),
-                          );
-                        }
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 20.0),
-                        child: ValueListenableBuilder(
-                          valueListenable: userName,
-                          builder: (BuildContext context, String? value, _) {
-                            if(value != null) {
-                              return Text(
-                                "Hello $value"
+                            ],
+                          ),
+                          ValueListenableBuilder(
+                            valueListenable: submitted,
+                            builder: (BuildContext context, bool value, _) {
+                              return Padding(
+                                padding: const EdgeInsets.only(top: 20),
+                                child: SolidTextButton(
+                                  text: "Sign In",
+                                  isLoading: value,
+                                  onPressed: () async {
+                                    if(value) return;
+                                    submitted.value = true;
+          
+                                    if(!formKey.currentState!.validate()) {
+                                      submitted.value = false;
+                                      return;
+                                    }
+          
+                                    User? user = await UserRemoteDatasource().signIn(User(
+                                      emailId: emailController.text,
+                                      password: passwordController.text
+                                    ));
+                                    submitted.value = false;
+                                    print(user?.name);
+                                    userName.value = user?.name;
+                    
+                                  },
+                                ),
                               );
                             }
-                            else {
-                              return Container();
-                            }
-                          },
-                        ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20.0),
+                            child: ValueListenableBuilder(
+                              valueListenable: userName,
+                              builder: (BuildContext context, String? value, _) {
+                                if(value != null) {
+                                  return Text(
+                                    "Hello $value"
+                                  );
+                                }
+                                else {
+                                  return Container();
+                                }
+                              },
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
-                ),
+                ],
               ),
-            ],
+            ),
           )),
     );
   }
