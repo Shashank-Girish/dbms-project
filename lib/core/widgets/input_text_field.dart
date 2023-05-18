65754a0c6a9a75fa447d3e5ed8cde065a282c7f9
Anirudh V Gubbi <anirudhvgubbi@gmail.com>
2023-05-19 01:02:22 +0530

    added keyboard type as param

--- lib/core/widgets/input_text_field.dart
+++ lib/core/widgets/input_text_field.dart
@@ -4,6 +4,7 @@ class InputTextField extends StatelessWidget {
   final int maxLines;
   final int maxLength;
   final String hintText;
+  final TextInputType keyboardType;
   final TextEditingController textEditingController;
   final String validatorMessage;
   final bool Function(String?) validator;
@@ -16,6 +17,7 @@ class InputTextField extends StatelessWidget {
       required this.textEditingController,
       required this.width,
       required this.validator,
+      this.keyboardType = TextInputType.text,
       this.validatorMessage = '',
       this.maxLength = 0,
       this.errorColor = Colors.red})
