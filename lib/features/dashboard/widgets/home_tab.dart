a436c06aa53d5467fbe9ed2e0f701ef321b699e1
Anirudh V Gubbi <anirudhvgubbi@gmail.com>
2023-05-30 16:50:45 +0530

    added rental params input functionality

---
+++ lib/features/dashboard/widgets/home_tab.dart
@@ -0,0 +1,305 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:vehicle_rental/connector/location_connector.dart';
+import 'package:vehicle_rental/core/colors.dart';
+import 'package:vehicle_rental/core/widgets/messages.dart';
+import 'package:vehicle_rental/core/widgets/solid_text_button.dart';
+import 'package:vehicle_rental/core/widgets/tab_button.dart';
+import 'package:vehicle_rental/models/location_model.dart';
+
+class HomeTab extends StatefulWidget {
+  const HomeTab({Key? key}) : super(key: key);
+
+  @override
+  State<HomeTab> createState() => _HomeTabState();
+}
+
+class _HomeTabState extends State<HomeTab> {
+  ValueNotifier<int> selectedTab = ValueNotifier(0);
+  List<ValueNotifier<String?>> selectedLocations = [
+    ValueNotifier(null),
+    ValueNotifier(null)
+  ];
+  ValueNotifier<DateTime?> selectedDate = ValueNotifier(null);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Column(
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 20.0, horizontal: 24.0),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 80.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          child: const Padding(
+                            padding: EdgeInsets.only(bottom: 54.0),
+                            child: Text(
+                              "High Service\nFor Every\nCustomer!",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 60),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 20.0),
+                          child: SolidTextButton(
+                            text: "Leave us a Review",
+                            buttonColor: kBlueButtonColor,
+                            onPressed: () {},
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 180,
+                      child: Image.asset(
+                        "assets/map_bg.png",
+                        fit: BoxFit.fitHeight,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              constraints: const BoxConstraints(minHeight: 288),
+              height: MediaQuery.of(context).size.height * 0.4,
+              width: double.maxFinite,
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      colors: kDashboardLinearGradientColors)),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 60.0, left: 160),
+                child: searchVehicleTabs(),
+              ),
+            )
+          ],
+        ),
+        Positioned(
+          top: 0,
+          right: MediaQuery.of(context).size.width * 0.0,
+          child: IgnorePointer(
+            child: SizedBox(
+              height: 600,
+              width: 600,
+              child: Image.asset(
+                "assets/car_model.png",
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget searchVehicleTabs() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ValueListenableBuilder(
+            valueListenable: selectedTab,
+            builder: (context, value, _) {
+              return ClipRRect(
+                borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(10.0),
+                    topRight: Radius.circular(10.0)),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TabButton(
+                      label: "4-Wheelers",
+                      onPressed: () {
+                        if (value != 0) {
+                          selectedTab.value = 0;
+                        }
+                      },
+                      isSelected: value == 0,
+                    ),
+                    TabButton(
+                      label: "2-Wheelers",
+                      onPressed: () {
+                        if (value != 1) {
+                          selectedTab.value = 1;
+                        }
+                      },
+                      isSelected: value == 1,
+                    )
+                  ],
+                ),
+              );
+            }),
+        ValueListenableBuilder(
+            valueListenable: selectedTab,
+            builder: (context, value, _) {
+              return ClipRRect(
+                borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(10.0),
+                    bottomRight: Radius.circular(10.0),
+                    topRight: Radius.circular(10.0)),
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.7,
+                  decoration: const BoxDecoration(color: kTabButtonColorActive),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 20.0, horizontal: 24.0),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        ValueListenableBuilder(
+                            valueListenable: selectedLocations[0],
+                            builder: (BuildContext context, String? value, _) {
+                              return markerWidget("Pick Up Location", value,
+                                  Icons.location_on_outlined, 0);
+                            }),
+                        const Spacer(),
+                        ValueListenableBuilder(
+                            valueListenable: selectedLocations[1],
+                            builder: (BuildContext context, String? value, _) {
+                              return markerWidget("Drop Off Location", value,
+                                  Icons.location_on_outlined, 1);
+                            }),
+                        const Spacer(),
+                        ValueListenableBuilder(
+                            valueListenable: selectedDate,
+                            builder:
+                                (BuildContext context, DateTime? value, _) {
+                              return markerWidget(
+                                  "Pick Up Date",
+                                  value?.toString().split(" ")[0],
+                                  Icons.date_range_outlined,
+                                  2);
+                            }),
+                        const Spacer(),
+                        SolidTextButton(
+                          text: "Search Rentals",
+                          buttonColor: kBlueButtonColor,
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            })
+      ],
+    );
+  }
+
+  Widget markerWidget(String label, String? value, IconData icon, int index) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: const TextStyle(color: Colors.black),
+        ),
+        InkWell(
+          onTap: () {
+            if (index == 0 || index == 1) {
+              showLocationDialog(context, index);
+            } else if (index == 2) {
+              showCalenderDialog(context);
+            }
+          },
+          child: MouseRegion(
+            cursor: SystemMouseCursors.click,
+            child: Row(
+              children: [
+                Icon(
+                  icon,
+                  color: Colors.black,
+                ),
+                Text(value ?? label.split(" ").last,
+                    style: TextStyle(color: Colors.grey[800]))
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  void showLocationDialog(BuildContext context, int locationIndex) {
+    showCupertinoDialog(
+        barrierDismissible: true,
+        useRootNavigator: false,
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(
+              title: const Text("Choose a Start Location"),
+              content: Container(
+                constraints: BoxConstraints(
+                    maxHeight: MediaQuery.of(context).size.height * 0.6),
+                child: SingleChildScrollView(
+                  child: FutureBuilder(
+                    future: LocationRemoteDatasource.getAllLocations(),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<Map<String, dynamic>> snapshot) {
+                      if (snapshot.hasError ||
+                          (snapshot.hasData && !snapshot.data!["success"])) {
+                        return const Text(kSomethingWentWrongMessage);
+                      }
+
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+
+                      return Material(
+                        color: Colors.transparent,
+                        child: StatefulBuilder(builder: (context, setState) {
+                          return Column(children: [
+                            for (int i = 0;
+                                i < snapshot.data!["data"].length;
+                                i++) ...{
+                              RadioListTile<String?>(
+                                groupValue:
+                                    selectedLocations[locationIndex].value,
+                                value: snapshot.data!["data"][i].name,
+                                onChanged: (value) {
+                                  setState(() {
+                                    selectedLocations[locationIndex].value =
+                                        value;
+                                  });
+                                },
+                                title: Text(
+                                  snapshot.data!["data"][i].name,
+                                  style: const TextStyle(fontSize: 18),
+                                ),
+                              )
+                            }
+                          ]);
+                        }),
+                      );
+                    },
+                  ),
+                ),
+              ));
+        });
+  }
+
+  void showCalenderDialog(BuildContext context) async {
+    selectedDate.value = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2023),
+      lastDate: DateTime(2030),
+    );
+  }
+}
