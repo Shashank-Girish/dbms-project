d255fe3bd4750263a0e1994e7431ed59062c1348
Shashank-Girish <shashankg.211cs256@nitk.edu.in>
2023-05-30 23:59:58 +0530

    Services tab done

--- lib/features/dashboard/widgets/services_tab.dart
+++ lib/features/dashboard/widgets/services_tab.dart
@@ -9,6 +9,70 @@ class ServicesTab extends StatelessWidget {
     return Container(
       height: 600,
       color: kBackgroundColor,
+      child: Column(children: [
+        const Text(
+          "\nWhat services we offer",
+          style: TextStyle(
+              fontSize: 40,
+              color: Colors.greenAccent,
+              fontFamily: "Neonderthaw"),
+        ),
+        Container(
+          padding: EdgeInsets.fromLTRB(
+              10, 2, MediaQuery.of(context).size.width / 2, 0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: const [
+              Text(
+                "Inter City Rentals",
+                style: TextStyle(
+                    fontSize: 35,
+                    color: Colors.blue,
+                    fontFamily: "BrushScript"),
+              ),
+              Text(
+                """We provide inter city car and bike rentals
+for your day to day city commutes and other needs.
+We also provide specials cars for special occasions.""",
+                style: TextStyle(fontSize: 25),
+              )
+            ],
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.fromLTRB(
+              MediaQuery.of(context).size.width / 2, 2, 10, 0),
+          child: Column(
+            children: const [
+              Text("\nIntra City Rentals",
+                  style: TextStyle(
+                      fontSize: 35,
+                      color: Colors.blue,
+                      fontFamily: "BrushScript")),
+              Text("""Specifically for your trips and vacations,
+we provide rentals for city to city travel with 
+seasoned cars to enhance and make your trip memorable.""",
+                  style: TextStyle(fontSize: 25)),
+            ],
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.fromLTRB(
+              10, 2, MediaQuery.of(context).size.width / 2, 0),
+          child: Column(
+            children: const [
+              Text("\nAirport Rentals",
+                  style: TextStyle(
+                      fontSize: 35,
+                      color: Colors.blue,
+                      fontFamily: "BrushScript")),
+              Text("""We provide rentals for airport pickups and dropoffs
+to make your travel experience seamless and stress free.""",
+                  style: TextStyle(fontSize: 25)),
+            ],
+          ),
+        ),
+      ]),
     );
   }
 }
